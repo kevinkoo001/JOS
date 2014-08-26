3f609af7d9ab0c9786dd9eace9115c9b01cd1e87
Don Porter <porter@cs.stonybrook.edu>
2014-08-25 23:26:06 -0400

    Lab 2

--- kern/init.c
+++ kern/init.c
@@ -9,21 +9,11 @@
 #include <kern/console.h>
 #include <kern/kdebug.h>
 #include <kern/dwarf_api.h>
+#include <kern/pmap.h>
+#include <kern/kclock.h>
 
 uint64_t end_debug;
 
-
-// Test the stack backtrace function (lab 1 only)
-void
-test_backtrace(int x)
-{
-	cprintf("entering test_backtrace %d\n", x);
-	if (x > 0)
-		test_backtrace(x-1);
-	else
-		mon_backtrace(0, 0, 0);
-	cprintf("leaving test_backtrace %d\n", x);
-}
 
 void
 i386_init(void)
@@ -46,9 +36,8 @@ i386_init(void)
     extern char end[];
     end_debug = read_section_headers((0x10000+KERNBASE), (uintptr_t)end); 
 
-
-	// Test the stack backtrace function (lab 1 only)
-	test_backtrace(5);
+	// Lab 2 memory management initialization functions
+	x64_vm_init();
 
 	// Drop into the kernel monitor.
 	while (1)
